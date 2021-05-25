33cd39e053388c4463c91a862714c19f62604c35
Teo553 <81677136+Teo553@users.noreply.github.com>
2021-05-25 14:03:08 +0200

    Add files via upload

---
+++ Teo553#3988_AckaBot/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODI2NzA4Mjc2NDU1OTMxOTI2.YGQaFA.7pEcsRzE9Yy4NpsWPDU96sP-vSQ',
+        prefix: '%',
+        activity: 'Listening to Teo553#3988',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
